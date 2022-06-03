b886336cec47b5335a5a3bae1fff52f09cc36110
lwmfjc <lwmfjc@gmail.com>
2022-06-03 12:09:36 +0800

    service

---
+++ src/main/java/com/ly/mybatisplus/service/UserService.java
@@ -0,0 +1,10 @@
+package com.ly.mybatisplus.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.ly.mybatisplus.mapper.UserMapper;
+import com.ly.mybatisplus.pojo.User;
+
+public interface UserService extends IService<User>{
+
+}
---
+++ src/main/java/com/ly/mybatisplus/service/impl/UserServiceImpl.java
@@ -0,0 +1,10 @@
+package com.ly.mybatisplus.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.ly.mybatisplus.mapper.UserMapper;
+import com.ly.mybatisplus.pojo.User;
+import com.ly.mybatisplus.service.UserService;
+
+public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
+
+}
