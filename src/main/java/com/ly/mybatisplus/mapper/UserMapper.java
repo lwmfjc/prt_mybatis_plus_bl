58ccae64063a3395a5d737a9a227a1197c40c1b7
lwmfjc <lwmfjc@gmail.com>
2022-06-03 10:59:48 +0800

    mapper增删改查

--- src/main/java/com/ly/mybatisplus/mapper/UserMapper.java
+++ src/main/java/com/ly/mybatisplus/mapper/UserMapper.java
@@ -4,7 +4,15 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.ly.mybatisplus.pojo.User;
 import org.springframework.stereotype.Repository;
 
+import java.util.Map;
+
 //将这个类标记成持久层组件 处理测试类中红色下划线的问题
 @Repository
 public interface UserMapper extends BaseMapper<User> {
+    /**
+     * 根据id查询用户信息为map的集合
+     * @param id
+     * @return
+     */
+    Map<String,Object> selectMapById(Long id);
 }
--- src/test/java/MybatisPlusTest.java
+++ src/test/java/MybatisPlusTest.java
@@ -5,7 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @SpringBootTest(classes = MybatisPlusApplication.class)
 public class MybatisPlusTest {
@@ -13,10 +16,84 @@ public class MybatisPlusTest {
     private UserMapper userMapper;
 
     @Test
-    public void testSelect(){
+    public void testSelect() {
         //通过条件构造器查询list集合 null表示没有条件
         List<User> users = userMapper.selectList(null);
         users.forEach(System.out::println);
+    }
 
+    @Test
+    public void testInsert() {
+        User user = new User();
+        user.setName("小明");
+        user.setAge(11);
+        user.setEmail("xx@163.com");
+        int insertNum = userMapper.insert(user);
+        System.out.println("result:" + insertNum);
+        System.out.println("result:" + user);
+    }
+
+    @Test
+    public void testDelete() {
+        int result = userMapper.deleteById(1532542803866394625L);
+        System.out.println(result);
+    }
+
+    @Test
+    public void testDeleteByMap() {
+        Map<String, Object> hash = new HashMap<>();
+        hash.put("name", "Sandy");
+        hash.put("age", "1234");
+        int result = userMapper.deleteByMap(hash);
+        System.out.println(result);
+    }
+
+    @Test
+    public void testDeleteByIds() {
+        List<Long> ids = Arrays.asList(1L, 2L, 5L);
+        int result = userMapper.deleteBatchIds(ids);
+        System.out.println(result);
+    }
+
+    @Test
+    public void testUpdateById() {
+        User user = new User();
+        user.setId(5L);
+        user.setEmail("email被修改了");
+        int result = userMapper.updateById(user);
+        System.out.println(result);
+    }
+
+    @Test
+    public void testSelectById() {
+        User user = userMapper.selectById(3);
+        System.out.println(user);
+    }
+
+    @Test
+    public void testSelectByIds() {
+        List<User> users = userMapper.selectBatchIds(Arrays.asList(1L, 2L, 5L));
+        users.forEach(System.out::println);
+    }
+
+    @Test
+    public void testSelectMap() {
+        Map<String, Object> hashMap = new HashMap<>();
+        hashMap.put("name","Tom");
+        hashMap.put("age",18);
+        List<User> users = userMapper.selectByMap(hashMap);
+        users.forEach(System.out::println);
+    }
+
+    @Test
+    public void testSelectAll() {
+        List<User> users = userMapper.selectList(null);
+        users.forEach(System.out::println);
+    }
+
+    @Test
+    public void testSelectCustom() {
+        Map<String, Object> map = userMapper.selectMapById(2L);
+        System.out.println(map);
     }
 }
