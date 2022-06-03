401e88602e73706292a36880e831940b499e55c0
lwmfjc <lwmfjc@gmail.com>
2022-06-03 20:46:26 +0800

    格式化

--- src/test/java/MybatisPlusIServiceTest.java
+++ src/test/java/MybatisPlusIServiceTest.java
@@ -11,34 +11,40 @@ import java.util.List;
 
 @SpringBootTest(classes = MybatisPlusApplication.class)
 public class MybatisPlusIServiceTest {
+
     @Autowired
     private UserService userService;
+
     @Test
-    public void testList(){
+    public void testList() {
         //List<User> list = userService.list();
         long count = userService.count();
-        System.out.println("总条数："+count);
+        System.out.println("总条数：" + count);
+
     }
+
     @Test
-    public void batchInsert(){
-        List<User> users=new ArrayList<>();
-        for(int i=0;i<10;i++){
-            User user=new User();
-            user.setName("name"+i);
-            user.setEmail("email"+i);
+    public void batchInsert() {
+        List<User> users = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            User user = new User();
+            user.setName("name" + i);
+            user.setEmail("email" + i);
             users.add(user);
         }
 
         boolean b = userService.saveBatch(users);
-        System.out.println("result:"+b);
+        System.out.println("result:" + b);
+
     }
 
     @Test
-    public void insertTest(){
-        User user=new User();
+    public void insertTest() {
+        User user = new User();
         user.setName("张三");
         user.setAge(18);
         boolean save = userService.save(user);
-        System.out.println("结果:"+save);
+        System.out.println("结果:" + save);
     }
+
 }
